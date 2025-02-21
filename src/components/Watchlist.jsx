a3454bf496c16fe2576a54db797f4e4590b4c708
Siddharth <msiddharth298@gmail.com>
2025-02-21 13:30:12 +0530

    genre filters added

--- src/components/Watchlist.jsx
+++ src/components/Watchlist.jsx
@@ -1,34 +1,73 @@
 import React from 'react'
-import { useState } from 'react'
-
-
+import { useState, useEffect } from 'react'
+import genrearr from '../utilities/genre.js'
+// we are only using the genreids object from the genre.js file 
 
 function Watchlist({watchlist}) {
-
   const [search, setSearch] = useState('')
-
+  const [genreList, setGenreList] = useState([])
+  const [currgenre, setCurrGenre] = useState('All Genre')
+  // function for searching
+  
   function searchMovie(e){
     setSearch(e.target.value)
     console.log(search)
   }
 
-
-
+  // function to get genres and create filters
+  
+  useEffect(()=>{
+    let temp = watchlist.map((movie)=>{
+      return genrearr[movie.genre_ids[0]]
+    })
+    // to remove the repetitve genres in the temp array we use hash set method
+   // in js we have inbuilt method for set
+    temp = new Set(temp);
+  // console.log(temp)
+    setGenreList(["All Genre",...temp]) // make the temp set as array and spread out all the values
+  },[watchlist])
+  
+// NOTE: in map function when we use {} we need to use return keyword to return the value
+// if we use () we dont need to use return keyword so that there should be the exact thing that should we return there should not be any logic 
+// for eg: see map function usage in use Effect and genre filters
   return (
     <>
-      <div className='w-full flex justify-center items-center my-8 '>
-        {/*genere Filters*/}
-
-
-        {/*Search bar*/}
-        <div >
-          <input type="text" placeholder='Search Movies' onChange={searchMovie} value={search} className='h-[3rem] w-[25rem] p-2 bg-gray-200 outline-none border border-gray-300 text-size-2 text-[1.2rem]'/>
+    
+      <div className='container mx-auto p-6'>
+        {/*GENRE FILTERS*/}
+         {/* This genre shows only the genre of the movies added to the watchlist */}
+        <div className="flex flex-wrap justify-center gap-7 my-4">
+          {genreList.map((genre)=>(
+          <button
+           key={genre}
+            onClick={()=> setCurrGenre(genre)}
+            className={`px-4 py-2 text-sm font-semibold rounded-md transition-all duration-300 ${
+              currgenre === genre ? "bg-red-400    text-white" : "bg-gray-300 hover:bg-gray-400"
+            }`}
+            > 
+            {genre}
+            </button>
+          ))}
         </div>
+        
 
+
+        {/*SEARCH BAR*/}
+        <div className='flex justify-center my-6' >
+          
+          <input
+           type="text" 
+           placeholder='Search Movies'
+            onChange={searchMovie} 
+            value={search} 
+            className='h-[3rem] w-[25rem] p-2 bg-gray-200 outline-none border border-gray-300 text-size-2 text-[1.2rem]'/>
+        </div>
 
       </div>
 
-      {/*table*/}
+      
+
+      {/*MOVIE TABLE*/}
       <div className="w-full m-8 overflow-x-auto">
         <table className="w-full border-collapse rounded-lg overflow-hidden shadow-lg">
 
@@ -43,11 +82,19 @@ function Watchlist({watchlist}) {
           </thead>
 
           <tbody className='text-[1.2rem]'>
-               {/* filtering the watchlist based on the search and then printing the data in the table */}
+
+            
+               {/* Each time currgenre (state) or search(state) changes, the component re-renders, and the filtering logic is applied again.
+This results in the displayed movies being updated according to the current filters. */}
+
             {watchlist.filter((movieobj)=>{
+              if(currgenre==="All Genre" || currgenre===genrearr[movieobj.genre_ids[0]]) return movieobj
+            }) // for genre based filtering
+            .filter((movieobj)=>{
               return movieobj.title.toLowerCase().includes(search.toLowerCase())
-            }).map((movieobj) => (
-              <tr className='border-b-2'>
+            }) // for search based filtering
+            .map((movieobj) => ( 
+             <tr className='border-b-2'>
               
               <td className="px-6 py-4 flex items-center space-x-4">
                 <img className='h-[7rem] w[11rem]' src={`https://image.tmdb.org/t/p/original${movieobj.poster_path}`} alt="Movie" />
@@ -56,9 +103,10 @@ function Watchlist({watchlist}) {
               
               <td className="px-6 py-4 text-center">{movieobj.vote_average}</td>
               <td className="px-6 py-4 text-center">{movieobj.popularity}</td>
-              <td className="px-6 py-4 text-center">Action</td>
-              <td className="px-6 py-4 text-center text-red-500 hover:text-red-900 cursor-pointer">Remove</td>
-            </tr>
+              <td className="px-6 py-4 text-center">{genrearr[movieobj.genre_ids[0]]}</td>
+              {/* mostly 1st genre in the genre array is preffered */}
+              <td className="px-6 py-4 text-center text-red-500  "><span className='cursor-pointer hover:text-red-900'>Remove</span></td>
+             </tr>
             ))}
             
             
